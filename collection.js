619b4d44ce7d645226bd9d421f27bbbf41e515a3
VARSHA THIRUMAKIL <varsht612@gmail.com>
2016-10-16 13:53:36 -0400

    first commit

---
+++ collection.js
@@ -0,0 +1,61 @@
+var database, numberOfDocs;
+Users = new Meteor.Collection('users');
+
+
+if (Meteor.isServer) {
+  console.log("On server")
+  database = new MongoInternals.RemoteCollectionDriver('mongodb://localhost:27017/newSensor');
+  numberOfDocs = database.open('users').find().count()
+  console.log(numberOfDocs)
+  Meteor.publish('users', function () {
+    return Users.find();
+  });
+}
+
+if (Meteor.isClient) {
+
+  //This code only runs on the client
+  Meteor.subscribe('users', function(){
+    console.log(Users.find().fetch());
+  });
+
+    Template.users.helpers({
+    'testingTemp': function (){ 
+      return "List of all the express js ids in meteor client : " + Users.find().count();
+    },
+    'name': function (){ 
+      return Users.findOne({_id: this._id}).name;
+    },
+   'id': function (){ 
+      var user = Users.findOne({_id:this._id});
+      var sensorArr = user.sensors;
+      if(sensorArr){
+        console.log("yes")
+        var arr = [];
+        sensorArr.forEach(function(entry) {
+          if(entry.measurements){
+            console.log("cool")
+            entry.measurements.forEach(function(entry1){
+              arr.push(entry1.time);
+              console.log(arr)
+            });
+          }
+        });
+      }
+      return arr;
+     },
+
+  });
+    Template.list.helpers({
+    'list_names':function(){
+      return Users.find();
+      // data = Users.find().fetch();
+      // return data.name
+      // var li = [];
+      // data.forEach(function(entry) {
+      // li.push(entry.name)
+      // });
+      // return li;
+    },
+  });
+}
---
+++ routes.js
@@ -0,0 +1,13 @@
+Router.map(function () {
+  this.route('about'); 
+  this.route('home', {
+    path: '/',  
+  });
+  this.route('users', {
+    path: '/user/:_id',
+    data: function () {return Users.findOne({_id: this.params._id})},
+  });
+  this.route('list', {
+    path: '/list',
+  });
+});
