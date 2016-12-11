f44ace9c05426218ed6b0c35d2228598841d4298
VARSHA THIRUMAKIL <varsht612@gmail.com>
2016-12-11 01:04:20 -0500

    code clean

--- collection.js
+++ collection.js
@@ -1,121 +1,124 @@
 var database, numberOfDocs;
 Users = new Meteor.Collection('users');
 
-
 if (Meteor.isServer) {
-  console.log("On server")
-  database = new MongoInternals.RemoteCollectionDriver('mongodb://localhost:27017/newSensor');
-  numberOfDocs = database.open('users').find().count()
-  console.log(numberOfDocs)
-  Meteor.publish('users', function () {
-    return Users.find();
-  });
+    database = new MongoInternals.RemoteCollectionDriver('mongodb://localhost:27017/newSensor');
+    numberOfDocs = database.open('users').find().count()
+    Meteor.publish('users', function() {
+        return Users.find();
+    });
 }
 
 if (Meteor.isClient) {
 
-  //This code only runs on the client
-  Meteor.subscribe('users', function(){
-    console.log(Users.find().fetch());
-  });
+    //This code only runs on the client
+    Meteor.subscribe('users', function() {
+        console.log(Users.find().fetch());
+    });
 
     Template.users.helpers({
 
-    'name': function (){ 
-      return Users.findOne({_id: this.userObjFromRouter._id}).name;
-    },
-    'id': function (){ 
-      return Users.findOne({_id: this.userObjFromRouter._id})._id;
-    },
-    'sen': function (){ 
-      return this.sensor_name;
-     },
+        'name': function() {
+            return Users.findOne({
+                _id: this.userObjFromRouter._id
+            }).name;
+        },
+        'id': function() {
+            return Users.findOne({
+                _id: this.userObjFromRouter._id
+            })._id;
+        },
+        'sen': function() {
+            return this.sensor_name;
+        },
 
-         'list_names':function(){
-      return Users.find();
-    },
-    'testingTemp': function (){ 
-      return Users.find().count();
-    },
-    
+        'list_names': function() {
+            return Users.find();
+        },
+        'testingTemp': function() {
+            return Users.find().count();
+        },
 
-      'val': function (){  
-      var user = Users.findOne({_id:this.userObjFromRouter._id});
-      var sensorArr = user.sensors;
-      var sensorNameFromRouter = this.sensor_name;
-      if(sensorArr){
-        var arr = [];
-        sensorArr.forEach(function(entry) {
-          if(entry.sensor_name == sensorNameFromRouter) {
-            if(entry.measurements){
-              arr = entry.measurements
-          }
-        }
-        });
-      }
-      return arr;
-     },
+        'val': function() {
+            var user = Users.findOne({
+                _id: this.userObjFromRouter._id
+            });
+            var sensorArr = user.sensors;
+            var sensorNameFromRouter = this.sensor_name;
+            if (sensorArr) {
+                var arr = [];
+                sensorArr.forEach(function(entry) {
+                    if (entry.sensor_name == sensorNameFromRouter) {
+                        if (entry.measurements) {
+                            arr = entry.measurements
+                        }
+                    }
+                });
+            }
+            return arr;
+        },
 
-      'timestamp': function (){ 
-      var user = Users.findOne({_id:this.userObjFromRouter._id});
-      var sensorArr = user.sensors;
-      var sensorNameFromRouter = this.sensor_name;
-      if(sensorArr){
-        var arr = [];
-        sensorArr.forEach(function(entry) {
-          if(entry.sensor_name == sensorNameFromRouter) {
-           if(entry.measurements){
-             arr = entry.measurements
-          }
-          }
-        });
-      }
-      return arr;
-     },
-  });
+        'timestamp': function() {
+            var user = Users.findOne({
+                _id: this.userObjFromRouter._id
+            });
+            var sensorArr = user.sensors;
+            var sensorNameFromRouter = this.sensor_name;
+            if (sensorArr) {
+                var arr = [];
+                sensorArr.forEach(function(entry) {
+                    if (entry.sensor_name == sensorNameFromRouter) {
+                        if (entry.measurements) {
+                            arr = entry.measurements
+                        }
+                    }
+                });
+            }
+            return arr;
+        },
+    });
 
     Template.list.helpers({
-    'list_names':function(){
-      return Users.find();
-    },
-    'testingTemp': function (){ 
-      return Users.find().count();
-    },
-    'id': function (){ 
-      return Users.findOne({_id: this.userObjFromRouter._id})._id;
-    },
-  });
+        'list_names': function() {
+            return Users.find();
+        },
+        'testingTemp': function() {
+            return Users.find().count();
+        },
+        'id': function() {
+            return Users.findOne({
+                _id: this.userObjFromRouter._id
+            })._id;
+        },
+    });
 
     Template.recent.helpers({
-    'recent_list':function(){
-      return Users.find({}, {sort: {'created_at':-1}, limit:5});
-    },
-  });
+        'recent_list': function() {
+            return Users.find({}, {
+                sort: {
+                    'created_at': -1
+                },
+                limit: 3
+            });
+        },
+    });
 
 
     Template.sen.helpers({
-    'list_sen': function (){ 
-      var user = Users.findOne({_id:this.userObjFromRouter._id});
-      var sensorArr = user.sensors;
-      return sensorArr;
-     },
+        'list_sen': function() {
+            var user = Users.findOne({
+                _id: this.userObjFromRouter._id
+            });
+            var sensorArr = user.sensors;
+            return sensorArr;
+        },
 
-    'name': function (){ 
-      return Users.findOne({_id: this.userObjFromRouter._id}).name;
-    },
+        'name': function() {
+            return Users.findOne({
+                _id: this.userObjFromRouter._id
+            }).name;
+        },
 
-    // 'recent_val':function (){
-    //   var user = Users.findOne({_id:this.userObjFromRouter._id});
-    //   var sensorArr = user.sensors;
-    //   arr = [];
-    //   sensorArr.forEach(function(entry) {
-    //     arr = entry.measurements;
-    //     console.log("CHECK" + sensorArr.measurements)
-    //   });
-    //   return arr;
-    //   // return Users.findOne({}, {sort: {DateTime: -1, limit: 1}});
-    // },
-
-  });
+    });
 
 }
--- routes.js
+++ routes.js
@@ -1,30 +1,32 @@
-Router.map(function () { 
-  this.route('list', {
-    path: '/',  
-  });
+Router.map(function() {
+    this.route('list', {
+        path: '/',
+    });
 
-  this.route('users', {
-    path: '/senList/:_id/:sensor_name',
-    data: function(){
-      return {
-            userObjFromRouter: Users.findOne({_id: this.params._id}),
-            sensor_name: this.params.sensor_name
-          };
+    this.route('users', {
+        path: '/senList/:_id/:sensor_name',
+        data: function() {
+            return {
+                userObjFromRouter: Users.findOne({
+                    _id: this.params._id
+                }),
+                sensor_name: this.params.sensor_name
+            };
         }
-      });
+    });
 
-  this.route('sen', {
-    path: '/senList/:_id/',
-     data: function(){
-      return {
-            userObjFromRouter: Users.findOne({_id: this.params._id}),
-          };
+    this.route('sen', {
+        path: '/senList/:_id/',
+        data: function() {
+            return {
+                userObjFromRouter: Users.findOne({
+                    _id: this.params._id
+                }),
+            };
         }
-  });
+    });
 
-  this.route('recent', {
-    path: '/recent_users',
-  });
-
-
+    this.route('recent', {
+        path: '/recent_users',
+    });
 });
